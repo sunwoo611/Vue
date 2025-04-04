e9de08448a8ac43fc57067a07fcd335ee7e23215
sw_vi.xi <sunwoo0251@gmail.com>
2025-04-04 15:22:31 +0900

    app

---
+++ app.vue
@@ -0,0 +1,20 @@
+<template>
+  <button @click="reverse">reverse</button>
+  <p v-if="toggle">you can see</p>
+  <p>{{ toggle }}</p>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      toggle: true
+    }
+  },
+  methods: {
+    reverse() {
+      this.toggle = !this.toggle
+    }
+  }
+}
+</script>
